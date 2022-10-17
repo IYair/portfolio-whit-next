832f5daeb838a444f87a547cc45791f031ac986e
iYair CC <al046638@uacam.mx>
2022-10-17 14:56:34 -0500

    Add Create Route of the web aplication

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,21 @@
+import Link from "next/link";
 
 export const NavBar = () => {
-return (
+  return (
     <nav className="w-full fixed inset-x-0 bottom-14 z-20">
       <div className="">
         <ul className="font-thin tracking-widest text-sm mx-52 flex flex-row justify-between content-center border-b border-white text-white pb-2">
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>PORTOFLIO</li>
-          <li>COURSES</li>
-          <li>BLOG</li>
+          <li>
+            <Link href={`/`}>HOME</Link>
+          </li>
+          <li>
+            <Link href={`/About`}>ABOUT</Link>
+          </li>
+          <li><Link href={`/Portfolio`}>PORTFOLIO</Link></li>
+          <li><Link href={`/Courses`}>COURSES</Link></li>
+          <li><Link href={`/Blog`}>BLOG</Link></li>
         </ul>
       </div>
     </nav>
-  )
+  );
 };
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import Layout from "../components/layout";
+import Typewriter from "typewriter-effect";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <a className="absolute right-[8rem]  -bottom-2 z-10 pl-40">
+        <Image src="/me.png" height={900} width={700} alt="Yair" />
+      </a>
+      <a className="-z-10">
+        <Image
+          src="/NoiseTexturenoiseBackgound.png"
+          layout="fill"
+          alt="noise"
+        />
+      </a>
+      <div
+        className="absolute left-[12.5%] top-1/2 text-[64px] text-white font-extralight z-10 tracking-[.3em]	whitespace-pre-line
+      drop-shadow-[0_0_10px_rgba(255,255,255,1)]
+      "
+      >
+        <Typewriter
+          options={{
+            strings: ["PORTFOLIO"],
+            cursor: "_",
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
